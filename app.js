cce67f62ff01686b9ae6d02daf810bd4466ad702
GrayBurford <GrayBurford@Gmail.com>
2021-10-06 17:18:40 -0400

    added JS Listeners

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const parent = document.getElementById('parent');
 const grandparent = document.getElementById('grandparent');
 
 
-const change = function changeBackground (e) {
-    console.log(e.srcElement.id)
-}
+// const change = function changeBackground (e) {
+//     console.log(e.srcElement.id);
+// }
 
-child.addEventListener('click', change);
-parent.addEventListener('click', change);
-grandparent.addEventListener('click', change);
-document.addEventListener('click', function () {
-    console.log('This is the body')
-})
+child.addEventListener('click', function () {
+    child.style.backgroundColor = 'purple';
+});
+
+parent.addEventListener('click', function () {
+    parent.style.backgroundColor = 'pink';
+});
+
+grandparent.addEventListener('click', function () {
+    grandparent.style.backgroundColor = 'magenta';
+});
+
+// document.addEventListener('click', function () {
+//     console.log('This is the body')
+// })
